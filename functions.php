6ba8b1b5909af0c697ec0c544a7cb409ec1fe3cc
Jacob Arriola <jacob.arriola@gmail.com>
2015-01-30 18:02:34 -0800

    Enqueue slick

--- functions.php
+++ functions.php
@@ -103,6 +103,9 @@ add_action( 'wp_enqueue_scripts', 'thepark_theme_styles');
 
 function thepark_scripts() {
 
+	// Add slick.js
+	wp_enqueue_script('slick', get_template_directory_uri() . '/app/components/slick.js/slick/slick.min.js')
+
 	wp_enqueue_script( 'thepark-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'thepark-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
